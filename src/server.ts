3011e70a11f60261fc261805b087b555c04cba30
DaeHyeonHong <fholng@outlook.com>
2023-01-02 23:07:36 +0900

    express-db-connect

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import {AppDataSource} from "./data-source";
 
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 app.use(morgan(`dev`));
 app.get("/", (_, res) => res.send("running"));
 
 let port = 4000;
 app.listen(port, async () => {
     console.log(`Server running at http://localhost:${port}`);
+    AppDataSource.initialize().then(async () => {
+        console.log("database Initialized.")
+    }).catch(error => console.log(error))
+
 })
